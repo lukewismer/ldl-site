f800308c268ddeb7802d9a2d8a919fbaac9dfdea
lukewismer <lukewismer77@gmail.com>
2023-01-19 22:05:37 -0700

    Home V1

--- src/Home/components/Pillars.js
+++ src/Home/components/Pillars.js
@@ -1,31 +1,44 @@
-import { PillarCard, PillarImage, PillarsContainer, PillarTitle, PillarHeader } from "../styles/Pillars.style";
+import { PillarCard, PillarImage, PillarsContainer, PillarTitle, PillarHeader, PillarCol, PillarRow } from "../styles/Pillars.style";
 
 const Pillars = () => {
     return (
         <PillarsContainer>
             <PillarHeader>4 Pillars Of The LDL</PillarHeader>
-            <PillarRow></PillarRow>
-            <IndPillarCard image={require("../../assets/team-pic-stock.jpeg")} title="Sportsmanship"></IndPillarCard>
+            <PillarRow>
+                <IndPillarCard image={require("../../assets/team-pic-stock.jpeg")} title="Sportsmanship" left={true}></IndPillarCard>
+                <IndPillarCard image={require("../../assets/team-pic-stock.jpeg")} title="Teamwork" left={false}></IndPillarCard>
+                <IndPillarCard image={require("../../assets/team-pic-stock.jpeg")} title="Love of the game" left={true}></IndPillarCard>
+                <IndPillarCard image={require("../../assets/team-pic-stock.jpeg")} title="Skill" left={false}></IndPillarCard>
+            </PillarRow>
+            
         </PillarsContainer>
     );
 }
 
-const PillarRow = ({image, title, desc}) => {
-    return (
-        <>
+const IndPillarCard = ({image, title, isLeft}) => {
+    if (isLeft)
+    {
+        return (
+            <PillarCol>
+                <PillarCard>
+                    <PillarImage src={image}></PillarImage>
+                    <PillarTitle>{title}</PillarTitle>
+                </PillarCard>
+            </PillarCol>
             
-        </>
-    )
-
-}
-
-const IndPillarCard = ({image, title}) => {
-    return (
-        <PillarCard>
-            <PillarImage src={image}></PillarImage>
-            <PillarTitle>{title}</PillarTitle>
-        </PillarCard>
-    )
+        )
+    } else {
+        return (
+            <PillarCol>
+                <PillarCard>
+                    <PillarTitle>{title}</PillarTitle>
+                    <PillarImage src={image}></PillarImage>
+                </PillarCard>
+            </PillarCol>
+            
+        )
+    }
+    
 }
 
 export default Pillars;
--- src/Home/styles/Pillars.style.js
+++ src/Home/styles/Pillars.style.js
@@ -2,18 +2,23 @@ import styled from "styled-components";
 
 
 export const PillarsContainer = styled.div`
-    background-color: #92A1B3;
+    background-color: #FFCCCB;
     margin: auto;
     margin-top: 10rem;
 `;
 
 export const PillarCard = styled.div`
-    box-shadow: 04px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    padding: 16px;
+    text-align: center;
+    background-color: #d1d1d1;
+    border-radius: 5%;
 `;
 
 export const PillarImage = styled.img`
-    height: 20rem;
+    height: 18rem;
+    width: 14rem;
+    border-radius: 20%;
 `;
 
 export const PillarTitle = styled.h2`
@@ -23,4 +28,22 @@ export const PillarTitle = styled.h2`
 export const PillarHeader = styled.h1`
     text-align: center;
     padding: 15px;
+`;
+
+export const PillarRow = styled.div`
+    margin: 0 -5px;
+    padding-left: 3%;
+    
+
+    &:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+`;
+
+export const PillarCol = styled.div`
+    float: left;
+    width: 23%;
+    padding: 0 10px;
 `;
